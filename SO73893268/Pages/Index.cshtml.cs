eafa27cfd2c34865353c340fcb3cb08c21ff2bca
jimmys20 <maragkosd12@gmail.com>
2022-09-29 13:26:25 +0300

    Add project files.

---
+++ SO73893268/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SO73893268.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPostUpdatePersonalData(string name, string surname, string mobile)
+        {
+            Console.WriteLine("");
+
+            return Page();
+        }
+    }
+}
